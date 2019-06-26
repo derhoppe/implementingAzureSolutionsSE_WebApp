64b89d7119c5e11966be0ecf9b1ca4ca979e5574
Sebastian <sebastian@sebastianhoppe.com>
2019-06-26 11:16:20 +0200

    Testkommentar hinzugefügt

--- PacktDemoApp/App_Start/RouteConfig.cs
+++ PacktDemoApp/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace PacktDemoApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+//Testkommentar
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
